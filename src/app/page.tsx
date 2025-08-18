9bdf18c9a53c8ef89de67f81cd7f0ab6d71bef5b
[lasy] <git@lasy.ai>
2025-08-18 15:04:19 -0300

    [lasy] App finalizado e funcionando perfeitamente - escreveu 1 arquivo(s)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
         return (
           <HomePage 
             onAction={handleButtonClick} 
-            onNavigate={navig ateToCategory} 
+            onNavigate={navigateToCategory} 
           />
         );
       
